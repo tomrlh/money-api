f43b044d6bc8bc9e291edea503cda5b3b83d86cb
Thiago Medeiros <si-thiago@hotmail.com>
2019-05-29 23:14:27 -0300

    Empty response, which code

--- src/main/java/moneyapi/resource/CategoriaResource.java
+++ src/main/java/moneyapi/resource/CategoriaResource.java
@@ -3,6 +3,7 @@ package moneyapi.resource;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,8 +19,9 @@ public class CategoriaResource {
 	private CategoriaRepository categoriaRepository;
 	
 	@GetMapping
-	public List<Categoria> listar() {
-		return categoriaRepository.findAll();
+	public ResponseEntity<?> listar() {
+		List<Categoria> categorias = categoriaRepository.findAll();
+		return !categorias.isEmpty() ? ResponseEntity.ok(categorias) : ResponseEntity.noContent().build();
 	}
 	
 }
