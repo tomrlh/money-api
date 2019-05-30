5397f1f716ee075f09b767175ab48d9bc88fc3e3
Thiago Medeiros <si-thiago@hotmail.com>
2019-05-30 14:38:36 -0300

    type: feat
    - rota /categorias POST
    - rota categorias/{id} GET

--- src/main/java/moneyapi/resource/CategoriaResource.java
+++ src/main/java/moneyapi/resource/CategoriaResource.java
@@ -1,12 +1,20 @@
 package moneyapi.resource;
 
+import java.net.URI;
 import java.util.List;
+import java.util.Optional;
+
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import moneyapi.model.Categoria;
 import moneyapi.repository.CategoriaRepository;
@@ -24,4 +32,26 @@ public class CategoriaResource {
 		return !categorias.isEmpty() ? ResponseEntity.ok(categorias) : ResponseEntity.noContent().build();
 	}
 	
+	@GetMapping("/{id}")
+	public Optional<Categoria> buscar(@PathVariable Long id) {
+//		Optional<Categoria> categoria = categoriaRepository.findById(id);
+//		return !categoria.isPresent() ? ResponseEntity.ok(categoria.get()) : ResponseEntity.noContent().build();
+		return categoriaRepository.findById(id);
+	}
+	
+	@PostMapping
+	//@ResponseStatus(HttpStatus.CREATED)
+	public ResponseEntity<Categoria> criar(@RequestBody Categoria categoria, HttpServletResponse response) {
+		Categoria categoriaSalva = categoriaRepository.save(categoria);
+		
+		URI uri = ServletUriComponentsBuilder
+			.fromCurrentRequestUri()
+			.path("/{codigo}")
+			.buildAndExpand(categoriaSalva.getCodigo())
+			.toUri();
+		
+		response.setHeader("Location", uri.toASCIIString());
+		
+		return ResponseEntity.created(uri).body(categoriaSalva);
+	}
 }
