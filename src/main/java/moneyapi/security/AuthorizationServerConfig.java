49030ce8d5abe220c0de57946f98eca7d3dbb201
Thiago Medeiros <si-thiago@hotmail.com>
2019-10-24 00:57:40 -0300

    Implemented RefreshTokenPostProcessor that implements ResponseBodyAdvice, that intercepts the Response body to remove the Refresh token and  moving it to an Cookie

--- src/main/java/moneyapi/security/AuthorizationServerConfig.java
+++ src/main/java/moneyapi/security/AuthorizationServerConfig.java
@@ -27,8 +27,9 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 			.withClient("angular")
 			.secret("angular")
 			.scopes("read", "write")
-			.authorizedGrantTypes("password")
-			.accessTokenValiditySeconds(1800);
+			.authorizedGrantTypes("password", "refresh_token")
+			.accessTokenValiditySeconds(20)
+			.refreshTokenValiditySeconds(3600*24);
 	}
 	
 	@Override
@@ -36,6 +37,7 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 		endpoints
 			.tokenStore(tokenStore())
 			.accessTokenConverter(accessTokenConverter())
+			.reuseRefreshTokens(false)
 			.authenticationManager(authenticationManager);
 	}
 	
