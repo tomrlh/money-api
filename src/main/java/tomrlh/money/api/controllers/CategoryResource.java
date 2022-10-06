2ce61a954f3032c461f7795f078ebf487d418fba
Thiago Medeiros (tomrlh) <ti2.dg@udv.org.br>
2022-10-06 00:35:35 -0300

    POST, FIND (id)

--- src/main/java/tomrlh/money/api/controllers/CategoryResource.java
+++ src/main/java/tomrlh/money/api/controllers/CategoryResource.java
@@ -1,11 +1,21 @@
 package tomrlh.money.api.controllers;
 
+import java.net.URI;
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import tomrlh.money.api.model.Category;
 import tomrlh.money.api.repository.CategoryRepository;
@@ -20,5 +30,23 @@ public class CategoryResource {
     @GetMapping
     public List<Category> findAll() {
         return categoryRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Category find(@PathVariable Long id) {
+        return categoryRepository.findById(id).get();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<Category> store(@RequestBody Category category, HttpServletResponse response) {
+        Category savedCategory = categoryRepository.save(category);
+
+        // adds to response header the Location URI of created resource
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}")
+            .buildAndExpand(savedCategory.getId()).toUri();
+        response.setHeader("Location", uri.toASCIIString());
+
+        return ResponseEntity.created(uri).body(savedCategory);
     }
 }
